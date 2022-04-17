53c4721e993576c8e7f0a31cf29f9458747dec91
Farhan <farhansafin@gmail.com>
2022-04-17 13:53:19 +0600

    Blogs page partially done

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h2>This is blog</h2>
+        <div className='m-5'>
+            <h2 className='text-center text-light bg-dark m-5'>Difference between authorization and authentication?</h2>
+            <p className='m-5'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In rerum dolor magnam fugiat accusamus quidem a unde, modi, ab quam architecto molestias nisi fugit voluptatem maxime minima quis voluptate distinctio?</p>
+            <h2 className='text-center text-light bg-dark m-5'>Why are you using firebase? What other options do you have to implement authentication?</h2>
+            <p className='m-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima libero suscipit dolor totam atque vero soluta, porro molestiae rerum nesciunt quisquam numquam. Saepe suscipit odit quod, quam neque maxime modi!</p>
+            <h2 className='text-center text-light bg-dark m-5'>What other services does firebase provide other than authentication?</h2>
+            <p className='m-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima libero suscipit dolor totam atque vero soluta, porro molestiae rerum nesciunt quisquam numquam. Saepe suscipit odit quod, quam neque maxime modi!</p>
         </div>
     );
 };
