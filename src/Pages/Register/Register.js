c1f2d7c9162c1b5327b4e55b2fea39f08120fed9
Farhan <farhansafin@gmail.com>
2022-04-17 17:08:07 +0600

    Registration and login checked and done

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,38 +1,58 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 import './Register.css'
 
 const Register = () => {
+
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
     const navigate = useNavigate();
 
     const navigateLogin = () => {
         navigate('/login')
     }
 
+    if(user){
+      alert('Successfull')
+    }
+
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
     const handleRegister = event => {
         event.preventDefault();
         const name = event.target.name.value;
-        const email = event.target.email.value;
-        const password = event.target.password.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(email, password);
+
+        createUserWithEmailAndPassword(email, password);
     }
 
     return (
         <div className='register-form'>
         <h2 style={{ textAlign: 'center' }} className='text-primary'>Please Register</h2>
         <Form onSubmit={handleRegister}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="formBasicName">
     <Form.Label>Name</Form.Label>
     <Form.Control className='text-primary' type="name" placeholder="Enter name" />
   </Form.Group>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control className='text-primary' type="email" placeholder="Enter email" required/>
+    <Form.Control ref={emailRef} className='text-primary' type="email" placeholder="Enter email" required/>
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control className='text-primary' type="password" placeholder="Password" required/>
+    <Form.Control ref={passwordRef} className='text-primary' type="password" placeholder="Password" required/>
   </Form.Group>
   <Button variant="primary" type="submit">
     Register
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apikey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+  apiKey: "AIzaSyBgwmDk5Wl6MK3DWKdb0Ugs0lAtrKmHkFw",
+  authDomain: "teaching-tutor-3cdb0.firebaseapp.com",
+  projectId: "teaching-tutor-3cdb0",
+  storageBucket: "teaching-tutor-3cdb0.appspot.com",
+  messagingSenderId: "30444254057",
+  appId: "1:30444254057:web:d3940d7c092da59edf03f8"
 };
 
 // Initialize Firebase
