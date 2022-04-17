683cc5fd115a50ea8806bb559404b6d2eb1b79a3
Farhan <farhansafin@gmail.com>
2022-04-17 15:18:42 +0600

    Login page done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from './Pages/Shared/Footer/Footer'
 import Login from './Pages/Login/Login';
 import Blogs from './Pages/Blogs/Blogs';
 import NotFound from './Pages/NotFound/NotFound';
+import Register from './Pages/Register/Register';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
